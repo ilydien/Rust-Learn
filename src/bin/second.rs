5d72bd8780aebb576d9795910d83d4610c906db2
Dien <akbardian64@gmail.com>
2026-05-13 18:43:30 +0700

    feat: add second rust turn

---
+++ src/bin/second.rs
@@ -0,0 +1,14 @@
+// fn main() {
+//     print!("hi, im my name is, ");
+//     print!("rn im learning rust");
+// }
+
+
+fn main() {
+    println!("hi, im my name is, ");
+    println!("rn im learning rust");
+}
+
+
+// print!(""); - untuk print yang masih dalam satu baris 
+// println!(""); - untuk print yang beda baris
