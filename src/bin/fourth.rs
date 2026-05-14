2d8d335df0f80d661bbbde12f1f10c3d7f80f7b5
Dien <akbardian64@gmail.com>
2026-05-14 12:47:04 +0700

    docs: add docs to fourth turn

--- src/bin/fourth.rs
+++ src/bin/fourth.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let myname: &str = "dien";
-    let myage: i32 = 19;
-    let myblodtype:  char =  'A';
-    let mywallet: f64 = 1.500;
+    let myname: &str = "dien"; //tipe data string 
+    let myage: i32 = 19; // tipe data integer (i8 s/d i128 - bisa juga depends on architecture (isize)) 
+    let myblodtype:  char =  'A'; // tipe data character
+    let mywallet: f64 = 1.500; // tipe data float
     print!("hi my name is {},\nim {} yo,\nmy blod type is {},\nand my wallet is {}", myname, myage, myblodtype, mywallet);
     
 }
