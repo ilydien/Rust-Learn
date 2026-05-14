3a988ed81a2a438dc868dcd9204b823ef7f1dfbd
Dien <akbardian64@gmail.com>
2026-05-14 12:38:03 +0700

    feat: add fourth rust turn

---
+++ src/bin/fourth.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let myname: &str = "dien";
+    let myage: i32 = 19;
+    let myblodtype:  char =  'A';
+    let mywallet: f64 = 1.500;
+    print!("hi my name is {},\nim {} yo,\nmy blod type is {},\nand my wallet is {}", myname, myage, myblodtype, mywallet);
+    
+}
